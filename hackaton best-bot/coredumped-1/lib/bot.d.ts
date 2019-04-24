a496f7002fa222db31e55fe241decea324390565
raular4322 <raular98@hotmail.com>
2019-04-24 14:43:44 +0200

    carpeta sobrante

--- hackaton best-bot/coredumped-1/lib/bot.d.ts
+++
@@ -1,9 +0,0 @@
-import { TurnContext } from 'botbuilder';
-export declare class MyBot {
-    /**
-     * Use onTurn to handle an incoming activity, received from a user, process it, and reply as needed
-     *
-     * @param {TurnContext} context on turn context object.
-     */
-    onTurn: (turnContext: TurnContext) => Promise<void>;
-}
