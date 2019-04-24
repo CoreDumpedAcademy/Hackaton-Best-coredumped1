3f753a64b8db322392448b08f10b3899e6b4f035
0smak <osmak@protonmail.com>
2019-04-24 14:14:43 +0200

    more phrases

--- hackaton-best-inteligencia/nlp.js/examples/console-bot/train-nlp.js
+++ hackaton-best-inteligencia/nlp.js/examples/console-bot/train-nlp.js
@@ -22,12 +22,20 @@
  */
 
 const fs = require('fs');
+const { NlpManager, ConversationContext } = require('node-nlp');
 
 module.exports = async function trainnlp(manager, say) {
   if (fs.existsSync('./model.nlp')) {
     manager.load('./model.nlp');
     return;
   }
+  const context = new ConversationContext();
+
+  manager.addNamedEntityText(
+    'nombre',
+    ['es'],
+    ['oscar', 'luis', 'alberto', 'juan', 'pedro', 'angela']
+  );
 
   manager.addDocument(
     'es',
@@ -45,9 +53,7 @@ module.exports = async function trainnlp(manager, say) {
   manager.addDocument('es', 'Hola, mi nombre es %nombre%', 'agent.asknombre');
   manager.addDocument('es', 'Me llamo %nombre%', 'agent.asknombre');
   manager.addDocument('es', 'Soy %nombre%', 'agent.asknombre');
-  manager.addDocument('es', '%nombre%', 'agent.asknombre');
 
-  manager.addDocument('es', '%apellidos%', 'agent.askapellidos');
   manager.addDocument('es', 'me apellido %apellidos%', 'agent.askapellidos');
   manager.addDocument(
     'es',
@@ -55,13 +61,66 @@ module.exports = async function trainnlp(manager, say) {
     'agent.askapellidos'
   );
 
-  manager.addDocument('es', '%dirección%', 'agent.askdireccion');
+  manager.addDocument('es', '%direccion%', 'agent.askdireccion');
   manager.addDocument('es', 'Vivo en %direccion%', 'agent.askdireccion');
-  manager.addDocument('es', 'Mi dirección', 'agent.askdireccion');
+  manager.addDocument(
+    'es',
+    'Mi dirección es %direccion%',
+    'agent.askdireccion'
+  );
+  manager.addDocument('es', 'Tengo %edad% años', 'agent.askedad');
+  manager.addDocument('es', 'Mi DNI es %dni%', 'agent.askdni');
+  manager.addDocument(
+    'es',
+    'Mi numero de telefono es %telefono%',
+    'agent.asktlfn'
+  );
+  manager.addDocument(
+    'es',
+    'Hay %miembros% en mi familia',
+    'agent.askmiembros'
+  );
+  manager.addDocument(
+    'es',
+    'En mi familia hay %miembros% miembros',
+    'agent.askmiembros'
+  );
+  manager.addDocument(
+    'es',
+    'El beneficiario es %beneficiario%',
+    'agent.askbeneficiario'
+  );
+  manager.addDocument('es', 'Mi ficha medica es ', 'agent.askficha');
+  manager.addDocument('es', 'si', 'enfermedadYES.askenfermedad');
+  manager.addDocument(
+    'es',
+    'la enfermedad es %enfermedad%',
+    'enfermedadYES.enfermedadRecibida'
+  );
+  manager.addDocument(
+    'es',
+    '¿Qué incluye la poliza?',
+    'enfermedadYES.enfermedadRecibida'
+  );
+  manager.addDocument(
+    'es',
+    '¿Qué incluye el seguro?',
+    'enfermedadYES.enfermedadRecibida'
+  );
+
+  manager.addDocument;
 
   say('Training, please wait..');
   const hrstart = process.hrtime();
-  await manager.train();
+  manager
+    .train()
+    .then(result =>
+      manager.process('es', 'Hola quiero una poliza de vida', context)
+    )
+    .then(result => manager.process('es', 'Me llamo Oscar', context))
+    .then(result => manager.process('es', 'Me apellido Muñoz', context))
+    .then(result => manager.process('es', 'Vivo en madrid', context))
+    .then(result => console.log(result.answer));
   const hrend = process.hrtime(hrstart);
   console.info('Trained (hr): %ds %dms', hrend[0], hrend[1] / 1000000);
   say('Trained!');
@@ -70,19 +129,21 @@ module.exports = async function trainnlp(manager, say) {
   manager.addAnswer(
     'es',
     'agent.asknombre',
-    'Hola {{nombre}} Por favor dime tus apellidos'
+    'Hola, {{nombre}}, Por favor dime tus apellidos'
   );
   manager.addAnswer(
     'es',
     'agent.askapellidos',
-    '¿Dónde vives? Por favor se lo más completo posible'
+    'Genial, {{nombre}} {{apellidos}} ¿Dónde vives? Por favor se lo más completo posible'
   );
   manager.addAnswer('es', 'agent.askdireccion', '¿Cuántos años tienes?');
   manager.addAnswer('es', 'agent.askedad', 'Introduzca su DNI por favor');
+  manager.addAnswer('es', 'agent.askdni', '¿Cuál es tu número de telefono?');
+
   manager.addAnswer(
     'es',
-    'agent.askdni',
-    '¿Cuántos miembros hay en su familia'
+    'agent.asktlfn',
+    '¿Cuántos miembros hay en su familia?'
   );
   manager.addAnswer(
     'es',
@@ -97,7 +158,19 @@ module.exports = async function trainnlp(manager, say) {
   manager.addAnswer(
     'es',
     'agent.askfichamedica',
-    '¿Hay alguna enfermedad hereditaria'
+    '¿Hay alguna enfermedad hereditaria en su familia?'
+  );
+
+  manager.addAnswer('es', 'enfermedadYES.askenfermedad', '¿Qué enfermedad?');
+  manager.addAnswer(
+    'es',
+    'enfermedadYES.enfermedadRecibida',
+    '¡Muchas gracias! trabajaré duro para hacerte tu poliza personalmente'
+  );
+  manager.addAnswer(
+    'es',
+    'enfermedadYES.incluye',
+    'Esta póliza incluye: fallecimiento por cualquier cosa, incapacidad permanente y enfermedades graves.'
   );
 
   manager.save('./model.nlp', true);
