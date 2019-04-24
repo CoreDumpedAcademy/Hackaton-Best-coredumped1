14e4841cbbb6493b37fb73e2c1173a6acd05d68d
0smak <osmak@protonmail.com>
2019-04-24 15:47:44 +0200

    saludos

--- hackaton-best-inteligencia/nlp.js/examples/console-bot/train-nlp.js
+++ hackaton-best-inteligencia/nlp.js/examples/console-bot/train-nlp.js
@@ -34,18 +34,15 @@ module.exports = async function trainnlp(manager, say) {
   }
   const context = new ConversationContext();
 
-  manager.addDocument(
-    'es',
-    'Hola, quiero hacerme una poliza de vida',
-    'agent.askpolizavida'
-  );
-  manager.addDocument(
-    'es',
-    'Me gustaria una poliza de vida',
-    'agent.askpolizavida'
-  );
+  manager.addDocument('es','Hola','agent.hola')
+
+  manager.addDocument('es','Hola, quiero hacerme una poliza de vida','agent.askpolizavida');
+  manager.addDocument('es', 'Me gustaria una poliza de vida', 'agent.askpolizavida');
   manager.addDocument('es', 'Quiero una poliza de vida', 'agent.askpolizavida');
   manager.addDocument('es', 'Quiero un seguro de vida', 'agent.askpolizavida');
+
+  manager.addDocument('es','quiero hacerme una poliza de decesos','agent.askpolizadecesos')
+  manager.addDocument('es','quiero hacerme un seguro de decesos','agent.askpolizadecesos')
 
   manager.addDocument('es', 'Hola, mi nombre es %nombre%', 'agent.asknombre');
   manager.addDocument('es', 'Me llamo %nombre%', 'agent.asknombre');
@@ -59,7 +56,7 @@ module.exports = async function trainnlp(manager, say) {
   );
 
   manager.addDocument('es', 'Vivo en %direccion%', 'agent.askdireccion');
-  
+
   manager.addDocument(
     'es',
     'Mi dirección es %direccion%',
@@ -105,22 +102,41 @@ module.exports = async function trainnlp(manager, say) {
     'enfermedadYES.enfermedadIncluye'
   );
 
-  manager.addDocument('es','Quiero eliminar un seguro','agent.eliminarSeguro')
-  manager.addDocument('es','Quiero eliminar una poliza','agent.eliminarSeguro')
-  manager.addDocument('es','Quiero darme de baja en un seguro','agent.eliminarSeguro')
-  manager.addDocument('es','Me gustaria darme de baja de una poliza','agent.eliminarSeguro')
-  manager.addDocument('es','Desearia darme de baja de el seguro contratado','agent.eliminarSeguro')
+  manager.addDocument(
+    'es',
+    'Quiero eliminar un seguro',
+    'agent.eliminarSeguro'
+  );
+  manager.addDocument(
+    'es',
+    'Quiero eliminar una poliza',
+    'agent.eliminarSeguro'
+  );
+  manager.addDocument(
+    'es',
+    'Quiero darme de baja en un seguro',
+    'agent.eliminarSeguro'
+  );
+  manager.addDocument(
+    'es',
+    'Me gustaria darme de baja de una poliza',
+    'agent.eliminarSeguro'
+  );
+  manager.addDocument(
+    'es',
+    'Desearia darme de baja de el seguro contratado',
+    'agent.eliminarSeguro'
+  );
 
-  manager.addDocument('es','He tenido un accidente','agent.acidente')
-  manager.addDocument('es','He sufrido un accidente','agent.acidente')
-  manager.addDocument('es','He provocado un accidente','agent.acidente')
-  manager.addDocument('es','He tenido un siniestro','agent.acidente')
+  manager.addDocument('es', 'He tenido un accidente', 'agent.acidente');
+  manager.addDocument('es', 'He sufrido un accidente', 'agent.acidente');
+  manager.addDocument('es', 'He provocado un accidente', 'agent.acidente');
+  manager.addDocument('es', 'He tenido un siniestro', 'agent.acidente');
 
-  manager.addDocument('es','Estoy en %localizacion%','agent.asklocalizacion')
-  manager.addDocument('es','No se donde estoy','agent.asklocalizacionNo')
-  manager.addDocument('es','no necesito nada','agent.necesidadNO')
-  manager.addAnswer('es','Sí %necesito%','agent.necesidadSI')
-  
+  manager.addDocument('es', 'Estoy en %localizacion%', 'agent.asklocalizacion');
+  manager.addDocument('es', 'No se donde estoy', 'agent.asklocalizacionNo');
+  manager.addDocument('es', 'no necesito nada', 'agent.necesidadNO');
+  manager.addAnswer('es', 'Sí %necesito%', 'agent.necesidadSI');
 
   manager.addDocument;
 
@@ -128,20 +144,27 @@ module.exports = async function trainnlp(manager, say) {
   const hrstart = process.hrtime();
 
   (async () => {
-    await manager.train()
-      .then(result => manager.process('es', 'Hola quiero una poliza de vida', context))
+    await manager
+      .train()
+      .then(result =>
+        manager.process('es', 'Hola quiero una poliza de vida', context)
+      )
       .then(result => manager.process('es', 'Me llamo Oscar', context))
       .then(result => manager.process('es', 'Me apellido Muñoz', context))
-      .then(result => manager.process('es', 'Vivo en madrid', context))
-      //.then(result => console.log(result.answer));
+      .then(result => manager.process('es', 'Vivo en madrid', context));
+    // .then(result => console.log(result.answer));
 
     manager.save();
     const response = await manager.process('es', 'Me llamo oscar');
-    //console.log(response);
+    // console.log(response);
   })();
   const hrend = process.hrtime(hrstart);
   console.info('Trained (hr): %ds %dms', hrend[0], hrend[1] / 1000000);
   say('Trained!');
+  
+  manager.addAnswer('es','agent.hola','¡Hola!')
+  manager.addAnswer('es','agent.hola','¡Hola! Bienvenido')
+  manager.addAnswer('es','agent.hola','¡Hola! Sera un placer atenderte')
 
   manager.addAnswer('es', 'agent.askpolizavida', '¡Hola! ¿Cómo te llamas?');
   manager.addAnswer(
@@ -191,20 +214,44 @@ module.exports = async function trainnlp(manager, say) {
     'Esta póliza incluye: fallecimiento por cualquier cosa, incapacidad permanente y enfermedades graves.'
   );
 
-  manager.addAnswer('es','agent.eliminarSeguro','¿Estás seguro? Es una lastima que te vayas')
+  manager.addAnswer(
+    'es',
+    'agent.eliminarSeguro',
+    '¿Estás seguro? Es una lastima que te vayas'
+  );
 
-  manager.addAnswer('es','agent.acidente','¿Sabes donde estás? Por favor indicamelo')
+  manager.addAnswer(
+    'es',
+    'agent.acidente',
+    '¿Sabes donde estás? Por favor indicamelo'
+  );
 
-  manager.addAnswer('es','agent.asklocalizacion','¿Necesitas algo más?')
-  
-  manager.addAnswer('es','agent.necesidadNO','¡Vale! Si necesitas algo dimelo')
-  manager.addAnswer('es','agent.necesidadSI','¡Genial! ¿Qué deseas?')
-  
-  manager.addAnswer('es','agent.buscarpoliza','Perfecto, voy a buscar tu número de poliza')
-  
-  manager.addAnswer('es','agent.enviargrua','Ya te he encontrado la poliza, enviaré una grúa y un mecanico a ayudarte')
+  manager.addAnswer('es', 'agent.asklocalizacion', '¿Necesitas algo más?');
 
-  manager.addAnswer('es','agent.asklocalizacionNo','Tranquilo, te encontraremos')
+  manager.addAnswer(
+    'es',
+    'agent.necesidadNO',
+    '¡Vale! Si necesitas algo dimelo'
+  );
+  manager.addAnswer('es', 'agent.necesidadSI', '¡Genial! ¿Qué deseas?');
+
+  manager.addAnswer(
+    'es',
+    'agent.buscarpoliza',
+    'Perfecto, voy a buscar tu número de poliza'
+  );
+
+  manager.addAnswer(
+    'es',
+    'agent.enviargrua',
+    'Ya te he encontrado la poliza, enviaré una grúa y un mecanico a ayudarte'
+  );
+
+  manager.addAnswer(
+    'es',
+    'agent.asklocalizacionNo',
+    'Tranquilo, te encontraremos'
+  );
 
   manager.save('./model.nlp', true);
 };
